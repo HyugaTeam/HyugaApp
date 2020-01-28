00803ddf31b02db2f18a63b840949818eaea799d
BlazeR23 <lord_of_the_funny@yahoo.com>
2020-01-29 00:35:06 +0200

    fullVersion0

---
+++ android/app/src/main/kotlin/com/hyuga/hyuga_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hyuga.hyuga_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
