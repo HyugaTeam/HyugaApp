35f1ec4ebac560995b7f2913bee064c83167e187
BlazeR23 <lord_of_the_funny@yahoo.com>
2020-02-09 00:08:24 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,11 @@ class Home extends StatelessWidget{
       left: 50,
       child: MyStatefulWidget(),
       ),
+      Positioned( 
+      top: 100,
+      left: 50,
+      child: MyStatefulWidget_1(),
+      ),
       ])
     );
   }
@@ -59,6 +64,45 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         });
       },
       items: <String>['Where','Coffee Shop', 'Restaurant', 'Pub']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+
+
+class MyStatefulWidget_1 extends StatefulWidget {
+  MyStatefulWidget_1({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState_1 createState() => _MyStatefulWidgetState_1();
+}
+
+class _MyStatefulWidgetState_1 extends State<MyStatefulWidget_1> {
+  String dropdownValue = 'Cuisine';
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['Cuisine','Mediterranian','American', 'Asian', 'Indian']
           .map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
           value: value,
