ebb2af603c40b967821786058f71ea8f9a6d023f
florianmarcu <florianmarcu@users.noreply.github.com>
2020-03-02 16:18:58 +0200

    List for the Locals on the SecondPage - Stable Version

--- lib/locals/local.dart
+++ lib/locals/local.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class Local{
   final int id;
-  final String name;
-  final String imageUrl;
-
+  final String name; // Imported from the database
+  final String imageUrl; // Imported from the database
+ 
   Local({
     this.id,
     this.name,
@@ -52,19 +54,53 @@ class Pub extends Local{
 
 Restaurant hanulLuiManuc = Restaurant(
   id: 1,
-  name: 'hanulLuiManuc',
-  imageUrl: 'assets/images/HanulLuiManuc.jpg',
+  name: 'Hanul lui Manuc', 
+  imageUrl: 'assets/images/HanulLuiManuc.jpg', 
 );
-Cafe bernschutz = Cafe(
+Restaurant caruCuBere =  Restaurant(
   id: 2,
-  name: 'bernschutz',
+  name: "Caru' cu bere",
+  imageUrl: 'assets/images/carucubere.jpg'
+);
+Restaurant hanuBerarilor = Restaurant(
+  id:3,
+  name : "Hanu' Berarilor",
+  imageUrl: 'assets/images/hanuberarilor.jpg'
+);
+
+Cafe bernschutz = Cafe(
+  id: 1,
+  name: 'Bernschutz&Co',
   imageUrl: 'assets/images/bernschutz.jpg',
+);
+Cafe secondCup = Cafe(
+  id: 2,
+  name: 'Second Cup Universitate',
+  imageUrl: 'assets/images/secondcup.jpg'
+); 
+Cafe cameraDinFata = Cafe(
+  id:3,
+  name: 'Camera din Fata',
+  imageUrl: 'assets/images/cameradinfata.jpg'
+);
+
+Pub kulturhaus = Pub(
+  id: 1,
+  name: 'Kulturhaus Bukarest',
+  imageUrl: 'assets/images/kulturhaus.jpg'
+);
+Pub silverChurch = Pub(
+  id: 2,
+  name: 'Silver Church',
+  imageUrl: 'assets/images/silverchurch.jpg'
 );
 Pub oktoberfest = Pub(
   id: 3,
-  name: 'oktoberfest',
+  name: 'Oktoberfest Pub',
   imageUrl: 'assets/images/oktoberfest.jpg',
 );
-List<Restaurant> restaurantsList = [hanulLuiManuc];
-List<Cafe> cafesList = [bernschutz];
-List<Pub> pubsList = [oktoberfest];
+
+List<Restaurant> restaurantsList = [hanulLuiManuc,caruCuBere,hanuBerarilor];
+List<Cafe> cafesList = [bernschutz,secondCup,cameraDinFata];
+List<Pub> pubsList = [oktoberfest,silverChurch,kulturhaus];
+var localsList = [cafesList,restaurantsList,pubsList];
--- lib/widgets/Second_Page.dart
+++ lib/widgets/Second_Page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hyuga_app/globals/Global_Variables.dart';
+import 'package:hyuga_app/locals/local.dart';
 
 class SecondPage extends StatefulWidget {
   @override
@@ -10,25 +12,54 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
+        iconTheme: IconThemeData(color: Theme.of(context).accentColor),
+        backgroundColor: Theme.of(context).backgroundColor,
         title: Text(
-          "Hello again"
+          "Hello again",
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+            fontWeight: FontWeight.bold
+          ),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: RaisedButton(
-            onPressed:(){ 
-              Navigator.pop(context);
-              },
-            child: Text(
-              'push me'
-              ),
-            ),
-         ),
-      )
+      body: Container(
+          padding: EdgeInsets.only(
+            top: 30,
+            left: 8,
+            right: 8
+          ),
+          child: ListView.builder(
+            itemCount: localsList[selectedWhere].length,
+            itemBuilder: (BuildContext contex, int index)  {
+              return MaterialButton(
+                onPressed:(){
+                  //Navigator.pop(context);
+                },
+                child: Container(
+                  //padding: EdgeInsets.all(20),
+                  height: 250,
+                  width: 380,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      image: AssetImage(
+                          localsList[selectedWhere][index].imageUrl
+                      )
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      
+                        ],
+                    )
+                  ),
+                );
+              }
+            )
+        ),
     );
   }
 }
