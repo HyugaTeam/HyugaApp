d3bdeabac55e2a79518a577117df6d72606aa4d4
florianmarcu <florian.marcu23@gmail.com>
2020-10-03 20:38:00 +0300

    Last Version of Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -17,84 +17,58 @@ exports.sendReservationNotification = functions.firestore
         const data = docSnapshot.data()
         //const reservationTime = data['date_start']
 
-        const querySnapshot = await db.collection('users').doc(data['guest_id'])
+        const querySnapshot = await docSnapshot.ref.parent.parent.parent.parent
         .collection('tokens').get();
+
+        // const querySnapshot = await db.collection('users').doc(data['guest_id'])
+        // .collection('tokens').get();
           
         const registrationTokens = querySnapshot.docs.map(ss => ss.id);
 
+        const time = dataBefore['date_start'].toDate()
         const payload = {
               notification: {
-                title: 'O noua rezervare a fost facuta',
-                body: data['guest_name'],
+                title: 'Rezervare noua!',
+                body: "O noua rezervare a fost facuta la data " + time,
                 clickAction: 'FLUTTER_NOTIFICATION_CLICK'
               }
             }
         return fcm.sendToDevice(registrationTokens,payload);
-        //return admin.messaging().sendToDevice(registrationTokens,payload)
-
-        // return admin.firestore().doc('users/'+ context.auth.uid).get().then(userDoc => {
-        //   const querySnapshot = await db.collection('users').doc(data.guest_id)
-        //   .collection('tokens').get();
-          
-        //   const registrationTokens = querySnapshot.docs.map(ss => ss.id);
-
-        //   const notificationBody = (message['type'] === 'TEXT') ? message['text'] : "You received a new reservation."
-
-        //   const payload = {
-        //     notification: {
-        //       title: 'O noua rezervare a fost facuta',
-        //       body: notificationBody,
-        //       clickAction: 'FLUTTER_NOTIFICATION_CLICK'
-        //     },
-        //     data:{
-        //       doc_id: userDoc.uid
-
-        //     }
-        //   }
-
-        //   return admin.messaging().sendToDevice(registrationTokens,payload)
-        // })
     })
 
 exports.sendReservationNotificationToUser = functions.firestore
-.document('users/{user}/reservation_history/{reservation}')
+.document('users/{user}/managed_locals/{managed_local}/reservations/{reservation}')
 .onUpdate(async (docSnapshot,context) => {
     const dataAfter = docSnapshot.after.data()
     const dataBefore = docSnapshot.before.data()
 
-    const querySnapshot = await db.collection('users').doc(data['guest_id'])
+    const querySnapshot = await db.collection('users').doc(dataBefore['guest_id'])
     .collection('tokens').get();
+    
+    const placeDoc = await docSnapshot.before.ref.parent.parent.get();
+    const placeName = placeDoc.data()['name'];
     
     const registrationTokens = querySnapshot.docs.map(ss => ss.id);
 
-    const time = dataBefore['date_start']
-    if(dataBefore['accepted'] == null && dataAfter['accepted'] == true){
+    const time = dataBefore['date_start'].toDate()
+    if(dataBefore['accepted'] === null && dataAfter['accepted'] === true){
       const payload = {
             notification: {
               title: 'Rezervare acceptata',
-              body: 'Rezervarea dumneavoastra la ' + dataAfter['place_name'] +' pentru ora '+ time+' a fost acceptata!',
+              body: 'Rezervarea dumneavoastra la ' + placeName +' pentru data '+ time +' a fost acceptata!',
               clickAction: 'FLUTTER_NOTIFICATION_CLICK'
             }
           }
       return fcm.sendToDevice(registrationTokens,payload);
     }
-    else {
+    else if(dataBefore['accepted'] === null && dataAfter['accepted'] === false){
       const payload = {
         notification: {
           title: 'Rezervare refuzata',
-          body: 'Rezervarea dumneavoastra la ' + dataAfter['place_name'] +' pentru ora '+ time+' a fost refuzata!',
+          body: 'Rezervarea dumneavoastra la ' + placeName + ' pentru data '+ time +' a fost refuzata!',
           clickAction: 'FLUTTER_NOTIFICATION_CLICK'
         }
       }
       return fcm.sendToDevice(registrationTokens,payload);
     }
 })
-
-// exports.setScoreToZero = functions.https.onRequest(async (req, res) => {
-//     // Grab the text parameter.
-//     const original = req.query.text;
-//     // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-//     const writeResult = await admin.firestore().collection('messages').add({original: original});
-//     // Send back a message that we've succesfully written the message
-//     res.json({result: `Message with ID: ${writeResult.id} added.`});
-//   });
