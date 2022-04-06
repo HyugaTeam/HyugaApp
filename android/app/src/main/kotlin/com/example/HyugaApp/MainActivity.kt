3096a9ffec3839cc79ea3c038467e92a50f1cb17
florianmarcu <florian.marcu23@gmail.com>
2022-04-06 16:26:28 +0300

    UpdatedAndroidFolderAfterRegeneratingFor2.2.0

---
+++ android/app/src/main/kotlin/com/example/HyugaApp/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.hyuga.hyuga_app
+
+import com.google.firebase.appcheck.FirebaseAppCheck
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
